b2384abf838db2500df2131c5db172aa6cf3da31
Cleanique Coders <cleanique.coders@gmail.com>
2016-06-28 15:02:50 +0800

    Fixed generate id

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,111 +1,31 @@
 angular.module('app.controllers', [])
 
-// controller(controller name, closure)
-// closure
-/*
-	function() {
-	
-	}
-*/
-// .controller('nameCtrl',function(){
+.run(function($rootScope, $localStorage, ClaimService){
+	ClaimService.initialize();
 
-// });
-.run(function($rootScope, $localStorage){
-
-	// bind to localStorage to rootScope
 	$rootScope.$storage = $localStorage;
-
-	if($rootScope.$storage.senarai == undefined) {
-		$rootScope.$storage.senarai = [];	
-	}
-	
-	// $rootScope.$storage.senarai = [
-	// 	{
-	// 		id:1,
-	// 		name:'Tuntutan Satu',
-	// 		description:'Tuntutan berkenaan....',
-	// 		amount: 25.50,
-	// 		photo: 'receipt-thumbnail.png'
-	// 	},
-	// 	{
-	// 		id:2,
-	// 		name:'Tuntutan Dua',
-	// 		description:'Tuntutan berkenaan....',
-	// 		amount: 3.50,
-	// 		photo: 'receipt-thumbnail.png'
-	// 	},
-	// 	{
-	// 		id:3,
-	// 		name:'Tuntutan Tiga',
-	// 		description:'Tuntutan berkenaan....',
-	// 		amount: 3.50,
-	// 		photo: 'receipt-thumbnail.png'
-	// 	}
-	// ];
-	//$localStorage.senarai = $rootScope.senarai;
 })
 
 .controller('tuntutanCtrl', function($scope) {
 
-	// $scope.title = 'new title';
-
-	// test change
-
 })
    
-.controller('tambahTuntutanCtrl', function($scope, $state) {
-
+.controller('tambahTuntutanCtrl', function($scope, $state, ClaimService) {
 	$scope.add = function() {
-		console.log($scope.tuntutan);
-
-		// save the tuntutan at the top of the list
-		$scope.$storage.senarai.unshift($scope.tuntutan);
-
-		var index = $scope.$storage.senarai.indexOf($scope.tuntutan);
-
-		if(index != -1) {
-			$scope.tuntutan.id = index + 1; // generate id
-			$scope.$storage.senarai[index] = $scope.tuntutan;
-		}
-		
-		// navigation to a particular page
-		// in this case, we go to tuntutan page
+		ClaimService.add($scope.tuntutan);
 		$state.go('tuntutan');
 	}
-
 })
    
-.controller('kemaskiniTuntutanCtrl', function($scope, $stateParams) {
-	$scope.tuntutan = $scope.$storage.senarai.reduce(function(carry, tuntutan){
-        if(tuntutan.id == $stateParams.id)
-            carry = tuntutan;
-        return carry;
-    }, {});
-
+.controller('kemaskiniTuntutanCtrl', function($scope, $stateParams, ClaimService) {
+	$scope.tuntutan = ClaimService.getClaimById($stateParams.id);
     $scope.tuntutan_ori = $scope.tuntutan;
 
     $scope.update = function() {
-    	var index = $scope.$storage.senarai.indexOf($scope.tuntutan_ori);
-
-    	if(index != -1) {
-    		$scope.$storage.senarai[index] = $scope.tuntutan;
-    	}
+    	ClaimService.update($scope.tuntutan_ori, $scope.tuntutan);
     }
 })
    
-.controller('perinciTuntutanCtrl', function($scope, $stateParams) {
-	// $stateParams
-		// access to parameters passed to the controller
-		// parameters configured in js/route.js
-
-	// Search function 
-		// to search the record based on id given
-		// can be replace with calling API
-		// to get informations
-	$scope.tuntutan = $scope.$storage.senarai.reduce(function(carry, tuntutan){
-        if(tuntutan.id == $stateParams.id)
-            carry = tuntutan;
-        return carry;
-    }, {});
+.controller('perinciTuntutanCtrl', function($scope, $stateParams, ClaimService) {
+	$scope.tuntutan = ClaimService.getClaimById($stateParams.id);
 })
- 
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,55 @@
 angular.module('app.services', [])
 
+.service('ClaimService',function($localStorage){
+	return {
+		initialize : function() {
+			if($localStorage.senarai == undefined) {
+				$localStorage.senarai = [];	
+			}
+		},
+		getClaims : function() {
+			return $localStorage.senarai;
+		},
+		getClaimById : function(id) {
+			return $localStorage.senarai.reduce(function(carry, tuntutan){
+		        if(tuntutan.id == id)
+		            carry = tuntutan;
+		        return carry;
+		    }, {});
+		},
+		add : function(tuntutan) {
+			console.log(tuntutan);
+			$localStorage.senarai.unshift(tuntutan);
+
+			var index = $localStorage.senarai.indexOf(tuntutan);
+
+			// need better way to generate id
+			var id = $localStorage.senarai.length;
+
+			if(index != -1) {
+				tuntutan.id = id; // generate id
+				$localStorage.senarai[index] = tuntutan;
+			}
+
+			delete tuntutan;
+		},
+		update : function(ori, tuntutan) {
+			var index = $localStorage.senarai.indexOf(ori);
+
+	    	if(index != -1) {
+	    		$localStorage.senarai[index] = tuntutan;
+	    	}
+		},
+		delete : function() {
+			var index = $localStorage.senarai.indexOf(ori);
+
+	    	if(index != -1) {
+	    		$localStorage.senarai.splice(index, 1);
+	    	}
+		}
+	}
+})
+
 .factory('BlankFactory', [function(){
 
 }])
