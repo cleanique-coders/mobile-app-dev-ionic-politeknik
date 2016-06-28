24ebb18439429dd0f0c6e1f2789aa53b350cb880
Cleanique Coders <cleanique.coders@gmail.com>
2016-06-28 14:17:46 +0800

    Done CRUD tuntutan using

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,29 +15,33 @@ angular.module('app.controllers', [])
 	// bind to localStorage to rootScope
 	$rootScope.$storage = $localStorage;
 
-	$rootScope.$storage.senarai = [
-		{
-			id:1,
-			name:'Tuntutan Satu',
-			description:'Tuntutan berkenaan....',
-			amount: 25.50,
-			photo: 'receipt-thumbnail.png'
-		},
-		{
-			id:2,
-			name:'Tuntutan Dua',
-			description:'Tuntutan berkenaan....',
-			amount: 3.50,
-			photo: 'receipt-thumbnail.png'
-		},
-		{
-			id:3,
-			name:'Tuntutan Tiga',
-			description:'Tuntutan berkenaan....',
-			amount: 3.50,
-			photo: 'receipt-thumbnail.png'
-		}
-	];
+	if($rootScope.$storage.senarai == undefined) {
+		$rootScope.$storage.senarai = [];	
+	}
+	
+	// $rootScope.$storage.senarai = [
+	// 	{
+	// 		id:1,
+	// 		name:'Tuntutan Satu',
+	// 		description:'Tuntutan berkenaan....',
+	// 		amount: 25.50,
+	// 		photo: 'receipt-thumbnail.png'
+	// 	},
+	// 	{
+	// 		id:2,
+	// 		name:'Tuntutan Dua',
+	// 		description:'Tuntutan berkenaan....',
+	// 		amount: 3.50,
+	// 		photo: 'receipt-thumbnail.png'
+	// 	},
+	// 	{
+	// 		id:3,
+	// 		name:'Tuntutan Tiga',
+	// 		description:'Tuntutan berkenaan....',
+	// 		amount: 3.50,
+	// 		photo: 'receipt-thumbnail.png'
+	// 	}
+	// ];
 	//$localStorage.senarai = $rootScope.senarai;
 })
 
@@ -49,7 +53,25 @@ angular.module('app.controllers', [])
 
 })
    
-.controller('tambahTuntutanCtrl', function($scope) {
+.controller('tambahTuntutanCtrl', function($scope, $state) {
+
+	$scope.add = function() {
+		console.log($scope.tuntutan);
+
+		// save the tuntutan at the top of the list
+		$scope.$storage.senarai.unshift($scope.tuntutan);
+
+		var index = $scope.$storage.senarai.indexOf($scope.tuntutan);
+
+		if(index != -1) {
+			$scope.tuntutan.id = index + 1; // generate id
+			$scope.$storage.senarai[index] = $scope.tuntutan;
+		}
+		
+		// navigation to a particular page
+		// in this case, we go to tuntutan page
+		$state.go('tuntutan');
+	}
 
 })
    
@@ -59,6 +81,16 @@ angular.module('app.controllers', [])
             carry = tuntutan;
         return carry;
     }, {});
+
+    $scope.tuntutan_ori = $scope.tuntutan;
+
+    $scope.update = function() {
+    	var index = $scope.$storage.senarai.indexOf($scope.tuntutan_ori);
+
+    	if(index != -1) {
+    		$scope.$storage.senarai[index] = $scope.tuntutan;
+    	}
+    }
 })
    
 .controller('perinciTuntutanCtrl', function($scope, $stateParams) {
